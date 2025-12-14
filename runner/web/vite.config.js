e6ef297db27232b66a2a1e77217fd5cfd42ade85
Anshra-dev <saiyadaanshraafzal@gmail.com>
2025-12-14 13:03:59 +0530

    Update vite.config.js

--- runner/web/vite.config.js
+++ runner/web/vite.config.js
@@ -1,11 +1,16 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '127.0.0.1',
     port: 5173,
+    strictPort: true
   },
+  preview: {
+    host: '127.0.0.1',
+    port: 4173,
+    strictPort: true
+  }
 });

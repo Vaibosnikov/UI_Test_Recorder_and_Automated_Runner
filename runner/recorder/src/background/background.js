6d5993a44a684155a7b2445f4771c4ac6d7d9fe4
Vaibhav Sharma <vaibhav_sharm@outlook.com>
2026-08-18 01:02:52 +0530

    refactor(recorder): Delete old runner/recorder/ directory
    - Old location superseded by new top-level recorder/
    - All files migrated in previous commit ff7dbf0
    - Completes structural reorganization

--- runner/recorder/src/background/background.js
+++
@@ -1,8 +0,0 @@
-﻿/**
- * runner/recorder/src/background/background.js
- * MV3 service worker
- */
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("TestCraft Recorder installed");
-});
